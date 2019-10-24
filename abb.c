d8bff85f0b084721717b7705880a0a5a6bea8b8e
florpatrone <patroneflorenciacarla@gmail.com>
2019-10-23 21:37:37 -0300

    Creada primitivas '_abb_obtener' y 'abb_obtener'

--- abb.c
+++ abb.c
@@ -1,4 +1,5 @@
 #define _POSIX_C_SOURCE 200809L 
+#define OBTENER_PADRE true
 #include "abb.h"
 #include <stdlib.h>
 #include <string.h>
@@ -22,6 +23,36 @@ struct abb{
 /***************************
 * Primitivas del ABB
 ****************************/
+
+nodo_t* _abb_obtener(nodo_t* nodo, const char* clave, abb_comparar_clave_t cmp, bool obtener_padre){
+	if (!nodo) return NULL;
+
+	int comparacion = cmp(nodo->clave,clave);
+
+	if (comparacion == 0){
+		return nodo;
+	}
+
+	if (comparacion > 0){
+        if (obtener_padre){
+			if (!nodo->izq) return NULL;
+
+			if (cmp(nodo->izq->clave,clave) == 0){
+				return nodo;
+			}
+		}
+		return _abb_obtener(nodo->izq,clave,cmp,obtener_padre);
+	}
+
+    if (obtener_padre){ //comparacion < 0
+		if (!nodo->der) return NULL;
+
+		if (cmp(nodo->der->clave,clave) == 0){
+			return nodo;
+		}
+	}
+	return _abb_obtener(nodo->der,clave,cmp,obtener_padre);
+}
 
 abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
     abb_t* abb = malloc(sizeof(abb));
@@ -48,10 +79,14 @@ abb_t* abb_crear(abb_comparar_clave_t cmp, abb_destruir_dato_t destruir_dato){
 }
 */
 
-/*void *abb_obtener(const abb_t *arbol, const char *clave){
+void *abb_obtener(const abb_t *arbol, const char *clave){
+	nodo_t* nodo = _abb_obtener(arbol->raiz,clave,arbol->cmp,!OBTENER_PADRE);
 
+	if (!nodo) return NULL;
+
+	return nodo->dato;
 }
-*/
+
 
 /*bool abb_pertenece(const abb_t *arbol, const char *clave){
 
